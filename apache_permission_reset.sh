c8d18a2380e6005a745872fc7569a1464a90df7e
rv8979 <45090896+rv8979@users.noreply.github.com>
2019-03-18 12:10:51 +0530

    Update apache_permission_reset.sh

--- apache_permission_reset.sh
+++ apache_permission_reset.sh
@@ -2,3 +2,14 @@ cd /var/www/
 sudo chgrp -R www-data html
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
+
+
+executable file 9 lines (7 sloc) 132 Bytes
+#!/bin/bash
+
+echo 'Enter the First Number :'
+read a
+echo 'Enter the Second Number :'
+read b
+x=$(expr "$a" + "$b")
+echo $a + $b = $x
